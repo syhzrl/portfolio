1dc6afc6fb8b6924e7102bce317f7eeb49d1b54d
syhzrl <syhzrl@gmail.com>
2022-10-08 20:42:02 +0800

    Route fix attempt #5

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -14,6 +14,7 @@ const HomeScreen: FunctionComponent = () => {
 
     useEffect(() => {
         console.log(location);
+        if (location.pathname === '/') NavActions.navToProfile();
     }, []);
 
     return (
